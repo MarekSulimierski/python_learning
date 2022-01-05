9800080ad1d8f655fc834296abdd75f969169493
MarekSulimierski <82377691+MarekSulimierski@users.noreply.github.com>
2022-01-05 15:15:47 +0100

    Update isValidChessBoard.py
    v. 0.3 dodano funkcję rysującą planszę (in progress)

--- venv/isValidChessBoard.py
+++ venv/isValidChessBoard.py
@@ -1,14 +1,13 @@
 import pprint
 
-chessBoard = {'a1': '', 'a2': '', 'a3': '', 'a4': '', 'a5': '', 'a6': '', 'a7': '', 'a8': '',
+chessBoard = {'a1': 'A1', 'a2': 'A2', 'a3': 'A3', 'a4': 'A4', 'a5': 'A5', 'a6': 'A6', 'a7': 'A7', 'a8': 'A8',
               'b1': '', 'b2': '', 'b3': '', 'b4': '', 'b5': '', 'b6': '', 'b7': '', 'b8': '',
               'c1': '', 'c2': '', 'c3': '', 'c4': '', 'c5': '', 'c6': '', 'c7': '', 'c8': '',}
 
 def validChessboard(board):
-    print(board)['a1'] + '|' + (board)['a2'] + '|' + (board)['a3'] + '|' + (board)['a4'] + '|' (board)['a5'] + '|'
-    + (board)['a6'] + '|' + (board)['a7'] + '|' + (board)['a8']
-    print('-------------------------------------------------------------------------------------------------------')
-
+    print(board['a1'] + '|' + board['a2'] + '|' + board['a3'] + '|' + board['a4'] + '|' + board['a5'] + '|'+ board['a6'] + '|' + board['a7'] + '|' + board['a8'])
+    #print('-------------------------------------------------------------------------------------------------------')
+validChessboard(chessBoard)
 
 for i in range (0,7):
     pawnSet = input('Podaj pole na planszy szachowej celem umieszczenia gońca(a1-h8): ')
@@ -16,10 +15,5 @@ for i in range (0,7):
         chessBoard[pawnSet] = "Pawn"
     elif pawnSet not in chessBoard:
         print("Nieprawidłowa pozycja. Podaj wartość w przedziale (a1:h8)")
-
+validChessboard(chessBoard)
 print(chessBoard)
-
-
-
-
-#pprint.pprint([chessBoard]
